3b5aba1d2250e6cf65afc2cddb0359fa81a90824
talhabekci <talhabekci@hotmail.com>
2022-03-01 13:41:48 +0300

    5.com

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,19 @@ var router = express.Router();
 const users = [];
 
 router.get('/', function(req, res, next) {
-  res.send(users);
+  res.send(
+      users
+        .sort((user1, user2) => {
+            if (user1.score < user2.score) {
+                return 1;
+            }
+            if (user1.score > user2.score) {
+                return -1;
+            }
+            return 0;
+        });
+        .slice(0,10)
+  );
 });
 
 router.post('/score', function(req, res, next) {
